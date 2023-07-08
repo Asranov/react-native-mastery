99bc25df76d5eb7deee4a6e02af888225750792f
Asranov <asranov0003@gmail.com>
2023-07-08 22:50:36 +0500

    refactored app.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import MyAnimatedComponent from './src/components/Interactions/MyAnimatedComponent';
-import MyAnimation from './src/components/Interactions/MyAnimation';
 import SimpleStatusBar from './src/components/SimpleStatusBar';
 
 function App(): JSX.Element {
@@ -9,7 +7,6 @@ function App(): JSX.Element {
   return (
     <SafeAreaView>
       <SimpleStatusBar />
-      <MyAnimation />
     </SafeAreaView>
   );
 }
