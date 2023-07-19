0f3afa338196ee8ecb7ef15dd3abf03b224c53f8
Asranov <asranov0003@gmail.com>
2023-07-19 18:12:17 +0500

    user added

--- src/components/Users/User.tsx
+++ src/components/Users/User.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
+import { Text, View } from 'react-native'
 
 
 interface User {
   name: string,
   age: number,
-  isMaried?: boolean
+  isMaried?: boolean | undefined
 }
 const User = () => {
-  const [user, setUser] = useState<User[]>([
+  const [users, setUsers] = useState<User[]>([
     {
       name: "Eshmat",
       age: 20,
@@ -20,9 +21,25 @@ const User = () => {
   ])
 
   return (
-    <>
-
-    </>
+    <View>
+      {users.map((user, index) => {
+        return (
+          <View key={index}>
+            <Text>{user.name}</Text>
+            <Text>{user.age}</Text>
+            {user.isMaried ? (
+              <>
+                <Text>User is maried</Text>
+              </>
+            ) : (
+              <>
+                <Text>User is not maried</Text>
+              </>
+            )}
+          </View>
+        )
+      })}
+    </View>
   )
 }
 
